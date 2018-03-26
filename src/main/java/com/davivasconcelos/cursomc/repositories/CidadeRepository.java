4a76566e8282a8d11b88afe3031aded2f79644ae
Davi de Melo <davimelovasc@gmail.com>
2018-03-25 22:31:18 -0300

    Estado e cidade criados

---
+++ src/main/java/com/davivasconcelos/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.davivasconcelos.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.davivasconcelos.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>  {
+
+}
