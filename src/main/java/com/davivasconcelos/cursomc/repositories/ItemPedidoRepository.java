5740caa87a9666838b53f6dabb906fc8d413cec2
Davi de Melo <davimelovasc@gmail.com>
2018-04-06 13:42:59 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/davivasconcelos/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.davivasconcelos.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.davivasconcelos.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>  {
+
+}
